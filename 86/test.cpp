2b27c7ba21c9299de61991caf1988701f7025b02
Qin.zehua <77593508@qq..com>
2026-04-08 20:00:25 +0800

    chore: update

--- 86/test.cpp
+++ 86/test.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <sstream>
 #include <thread>
+#if !defined(_MSC_VER)
+#include <time.h>
+#endif
 #if defined(_WIN32)
 #include <windows.h>
 #endif
@@ -18,12 +21,18 @@ int main() {
     while (true) {
         const auto now = std::chrono::system_clock::now();
         const std::time_t t = std::chrono::system_clock::to_time_t(now);
-        const std::tm* const p = std::localtime(&t);
-        if (!p) {
+        std::tm local{};
+#if defined(_MSC_VER)
+        if (localtime_s(&local, &t) != 0) {
             std::cerr << "localtime failed\n";
             return 1;
         }
-        std::tm local = *p;
+#else
+        if (localtime_r(&t, &local) == nullptr) {
+            std::cerr << "localtime failed\n";
+            return 1;
+        }
+#endif
         std::ostringstream oss;
         oss << std::put_time(&local, "%Y-%m-%d %H:%M:%S");
         std::cout << '\r' << oss.str() << std::flush;
