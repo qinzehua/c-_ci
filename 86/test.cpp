2e70eea3956bea490fcaecc07fe0dd6582c94417
Qin.zehua <77593508@qq..com>
2026-04-08 19:51:04 +0800

    chore: init

---
+++ 86/test.cpp
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+int main() {
+#if defined(_WIN32)
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    std::cout << "简易时钟 - 按 Ctrl+C 退出\n";
+
+    while (true) {
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t t = std::chrono::system_clock::to_time_t(now);
+        std::tm local{};
+#if defined(_WIN32)
+        localtime_s(&local, &t);
+#else
+        localtime_r(&t, &local);
+#endif
+        std::ostringstream oss;
+        oss << std::put_time(&local, "%Y-%m-%d %H:%M:%S");
+        std::cout << '\r' << oss.str() << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
